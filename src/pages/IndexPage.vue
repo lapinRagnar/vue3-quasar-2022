7165ed4595c29b794e3a7ad1e9496b6742e9da97
lapinRagnar <lapinragnar@gmail.com>
2022-09-09 09:46:09 +0200

    ajouter checkbox, un label, et une icon delete dans la page index

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,16 +1,51 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="row">
 
+    <q-card
+      class="col-xs-9"
+    >
+      <q-toolbar class="bg-amber-6">
+        <q-input :model-value="search"></q-input>
+      </q-toolbar>
 
-    <q-btn label="click moi"></q-btn>
+      <q-list>
+        <q-item>
+
+          <q-item-section side>
+            <q-checkbox :model-value="true"/>
+          </q-item-section>
+
+          <q-item-section>
+            faire les course!
+          </q-item-section>
+
+          <q-item-section side>
+            <q-btn
+              flat
+              icon="mdi-delete"
+              round
+              size="sm"
+              />
+          </q-item-section>
+
+        </q-item>
+      </q-list>
+
+    </q-card>
+
 
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+  data(){
+    return {
+      search: ''
+    }
+  }
+});
 </script>
